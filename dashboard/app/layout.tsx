29dfff0152c07b81296422877651abb524efcebb
Lucas Greenwell <54864050+lucasgreenwell@users.noreply.github.com>
2025-03-14 16:26:19 -0500

    Fix: header

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'SiSo Take Home',
+  description: 'Created by Lucas Greenwell',
 }
 
 export default function RootLayout({
